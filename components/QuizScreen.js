3d0b804740b6af92331658205c272c5709d0614f
Michael Selvesakis <miselve@cs.ihu.gr>
2024-05-16 01:06:20 +0300

    Fixed QuizScreen Functionality

--- components/QuizScreen.js
+++ components/QuizScreen.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
 export default function QuizScreen({ navigation }) {
-  const [resetScore, setResetScore] = useState(true);
+  const [quizStarted, setQuizStarted] = useState(false);
+  const [completed, setCompleted] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [score, setScore] = useState(0);
@@ -14,25 +15,26 @@ export default function QuizScreen({ navigation }) {
       correctAnswer: 'Paris'
     },
     {
-        question: 'What is the capital of France?',
-        answers: ['London', 'Paris', 'Berlin', 'Rome'],
-        correctAnswer: 'Paris'
-      },
-      {
-        question: 'What is the capital of France?',
-        answers: ['London', 'Paris', 'Berlin', 'Rome'],
-        correctAnswer: 'Paris'
-      },
-      {
-        question: 'What is the capital of France?',
-        answers: ['London', 'Paris', 'Berlin', 'Rome'],
-        correctAnswer: 'Paris'
-      },
+      question: 'What is the capital of UK?',
+      answers: ['London', 'Paris', 'Berlin', 'Rome'],
+      correctAnswer: 'London'
+    },
+    {
+      question: 'What is the capital of Greece?',
+      answers: ['London', 'Paris', 'Athens', 'Rome'],
+      correctAnswer: 'Athens'
+    },
+    {
+      question: 'What is the capital of Italy?',
+      answers: ['London', 'Paris', 'Berlin', 'Rome'],
+      correctAnswer: 'Rome'
+    }
     // Add more questions here...
   ];
 
   const handleStartQuiz = () => {
-    setResetScore(false);
+    setQuizStarted(true);
+    setCompleted(false);
     setCurrentQuestion(0);
     setSelectedAnswer(null);
     setScore(0);
@@ -44,30 +46,17 @@ export default function QuizScreen({ navigation }) {
 
   const goToNextQuestion = () => {
     const isCorrect = questions[currentQuestion].correctAnswer === selectedAnswer;
-    console.log('Selected Answer:', selectedAnswer);
-    console.log('Correct Answer:', questions[currentQuestion].correctAnswer);
-    console.log('Is Correct:', isCorrect);
-  
+
     if (isCorrect) {
-        const newScore = score + 10;
-        console.log('New Score:', newScore);
-        setScore(newScore);
-      }
-  
+      const newScore = score + 10;
+      setScore(newScore);
+    }
+
     if (currentQuestion < questions.length - 1) {
       setCurrentQuestion(currentQuestion + 1);
       setSelectedAnswer(null);
     } else {
-      // Navigate to the score screen and reset quiz state
-      if (score === 0) {
-        setScore(0);
-    } else if (selectedAnswer === questions[currentQuestion].correctAnswer) {
-        setScore(score + 10);
-    }
-    console.log('Final Score:', score);
-    // Navigate to the score screen and reset quiz state
-    navigation.navigate('Score', { score });
-    setResetScore(true);
+      setCompleted(true);
     }
   };
 
@@ -78,16 +67,26 @@ export default function QuizScreen({ navigation }) {
     }
   };
 
+  const handleViewScore = () => {
+    navigation.navigate('Score', { score });
+    setQuizStarted(false);
+    setCompleted(false);
+    setCurrentQuestion(0);
+    setSelectedAnswer(null);
+    setScore(0);
+  };
+
   return (
     <View style={styles.container}>
-      {resetScore ? (
-        <View style={styles.startQuizContainer}>
-          <Text style={styles.title}>Welcome to the Quiz</Text>
+      {!quizStarted && !completed && (
+        <>
+          <Text style={styles.title}>Ear Training Quiz</Text>
           <TouchableOpacity style={styles.button} onPress={handleStartQuiz}>
             <Text style={styles.buttonText}>Start Quiz</Text>
           </TouchableOpacity>
-        </View>
-      ) : (
+        </>
+      )}
+      {quizStarted && !completed && (
         <>
           <Text style={styles.question}>{questions[currentQuestion].question}</Text>
           {questions[currentQuestion].answers.map((answer, index) => (
@@ -115,11 +114,19 @@ export default function QuizScreen({ navigation }) {
             </Text>
             <TouchableOpacity
               onPress={goToNextQuestion}
-              disabled={selectedAnswer === null}
-            >
+              disabled={selectedAnswer === null}>
               <Text>Next</Text>
             </TouchableOpacity>
           </View>
+        </>
+      )}
+      {completed && (
+        <>
+          <Text style={styles.title}>Quiz Completed!</Text>
+          <TouchableOpacity style={styles.button} onPress={handleViewScore}>
+
+            <Text style={styles.buttonText}>View Score</Text>
+          </TouchableOpacity>
         </>
       )}
     </View>
@@ -132,24 +139,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  startQuizContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 30,
-  },
   button: {
     backgroundColor: '#007bff',
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 5,
+    marginTop: 20,
   },
   buttonText: {
     color: 'white',
     fontSize: 18,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 30,
   },
   question: {
     fontSize: 20,
